a68c303f4394287b33204d6f88b39e175fe0f8ef
ALittleDruid <297957399@qq.com>
2021-08-23 19:30:21 +0800

    捕获异常

--- GitHubIP/App.xaml.cs
+++ GitHubIP/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace GitHubIP
 {
@@ -7,5 +10,38 @@ namespace GitHubIP
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception ex)
+            {
+
+                MessageBox.Show($"捕获未处理异常1 {ex}");
+            }
+            else
+            {
+                MessageBox.Show($"捕获未处理异常2 {e.ExceptionObject}");
+            }
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            MessageBox.Show($"捕获未处理异常3 {e.Exception}");
+            e.SetObserved();
+        }
+
+        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show($"捕获未处理异常4 {e.Exception}");
+
+        }
     }
 }
